718deeb506b3a7d333a0b93d34513a6fddbf9e3e
AbdulazizDXT <105378232+AbdulazizDXT@users.noreply.github.com>
2022-08-09 20:14:08 +0400

    Create FixerScript.sh

---
+++ FixerScript.sh
@@ -0,0 +1,21 @@
+#Fix Source.list
+echo "deb http://kali.download/kali kali-rolling main contrib non-free
+deb http://kali.download/kali kali-last-snapshot main contrib non-free
+deb http://kali.download/kali kali-experimental main contrib non-free
+deb http://kali.download/kali kali-bleeding-edge main contrib non-free" | sudo tee /etc/apt/sources.list
+sudo apt-get update -y
+#Fix NetworkManager
+mv NetworkManager.conf /etc/NetworkManager/
+service NetworkManager restart
+#Fix Drivers TP-Link WN772
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo  apt-get dist-upgrade
+sudo apt-get install bc
+sudo apt-get install build-essential 
+sudo apt-get install libelf-dev 
+sudo apt-get install linux-headers-$(uname -r)
+sudo apt-get install dkms
+sudo rmmod r8188eu.ko
+git clone https://github.com/aircrack-ng/rtl8188eus
+cd rtl8188eus
+clear 
+cat Now
